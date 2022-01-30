b5451db86d0f59e5564e5cb388dee2a32b3502ad
mrGain <rakeshgain47681@gmail.com>
2022-01-30 20:49:08 +0530

    if_else date-30-jan

---
+++ first_year_2021-25/first_if.c
@@ -0,0 +1,100 @@
+/*
+
+#include<stdio.h>
+
+void main()
+{
+   int number;
+   printf("enter a number :");
+   scanf("%d",&number);
+
+   if(number % 2 == 0)
+   {
+       printf("%d is a even number .\n",number);
+   }
+   else
+   {
+       printf("%d is a odd number",number);
+   }
+}
+*/
+
+ // 2. WAP TO CHECK A NUMBER +VE OF -VE 
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a;
+//     printf("Enter a Number\n");
+//     scanf("%d", &a);
+
+//     if (a > 0)
+//     {
+//         printf("%d Number is Positive", a);
+//     }
+//     else if(a < 0 )
+//     {
+//         printf("%d Number is Negative", a);
+//     }
+//     else
+//     {
+//         printf("The entered number is zero");
+//     }
+
+//     return 0;
+// }
+
+
+// if else ladder
+
+// if(cond)
+// {
+
+// }
+// else if(cond.)
+// {
+
+// }
+// else if(cond.)
+// {
+
+// }
+// else if(cond.)
+// {
+
+// }
+// else{
+
+// }
+
+/*
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int flag;
+    printf("Enter a Number\n");
+    scanf("%d", &a);
+
+    if (a > 0)
+    {
+        printf("%d Number is Positive", a);
+        
+        scanf("%d", &flag);
+        printf("flag =%d", flag);
+
+    }
+    else if(a < 0 )
+    {
+        printf("%d Number is Negative", a);
+    }
+    else
+    {
+        printf("The entered number is zero");
+    }
+
+    return 0;
+}
+*/
